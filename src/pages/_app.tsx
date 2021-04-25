0251f47dbee3bf5664c7c07c5ca3f334788838c0
juliamendesc <juliamendesc@gmail.com>
2021-04-25 13:04:51 +0100

    podcastr

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+import { PlayerContextProvider } from '../../context/PlayerContext';
+import '../styles/global.scss';
+import styles from '../styles/app.module.scss';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <PlayerContextProvider>
+    <div className={styles.appWrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+    </PlayerContextProvider>
+  )
+}
+
+export default MyApp
